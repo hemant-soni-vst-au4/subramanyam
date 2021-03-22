9555ff47654a7721a9ec7cf021068d97667d100d
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-22 17:16:58 +0530

    index.html in week18day2

--- week18day1/index.js
+++ week18day1/index.js
@@ -1,10 +1,11 @@
-function add() {
+const add = () => {
     let x = parseInt(document.getElementById('number1').value);
     let y = parseInt(document.getElementById('number2').value);
-    console.log(x+y)
 
     document.getElementById('output').innerHTML = x+y;
     return false;
 }
 
 document.getElementById('sum').addEventListener('click', add);
+
+
