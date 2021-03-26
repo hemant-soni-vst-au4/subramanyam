6d8473a501c3d9c7c71e668f382604618d335a40
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-26 18:32:28 +0530

    week19day1

--- week19day1/index.js
+++ week19day1/index.js
@@ -1,22 +1,57 @@
-
-
 const backSpace = () => {
-    const input1 = document.querySelector("#input1").value;
-    let arr = []
-    console.log("input", input1);
+    let input1 = document.querySelector('#input1').value;
+    let arr = [];
     for (let i = 0; i < input1.length; i++) {
-        if (input1[i] === "#") {
-          arr.pop();
+        if (input1[i] === '#') {
+            arr.pop()
         } else {
-          arr.push(input1[i]);
-          console.log("arr", arr);
+            arr.push(input1[i])
         }
-
-      }
-      document.getElementById("output").innerHTML = arr.join(" ")
+        console.log("arr", arr)
+    }
+    document.querySelector("#output").innerHTML = arr.join("")
 }
 
-
-
-
 document.querySelector("#submit").addEventListener('click', backSpace);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
