3b284afff430f15f6d5a454a4f4fcae6979c468e
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-04-07 16:01:10 +0530

    week20day2

---
+++ week20day2/index.js
@@ -0,0 +1,99 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let users = [
+//     'Goku',
+//     'Naruto',
+//     'Ichigo',
+//     'Flash',
+//     'Batman',
+//     'Sherlock Holmes',
+//     'Khaleesi',
+//     'Steve Fox'
+//   ];
+  
+//   ul = document.getElementById("users-list");
+  
+//   let render_lists = function(lists){
+//     let li = "";
+//     for(index in lists){
+//       li += "<li>" + lists[index] + "</li>";
+//     }
+//     ul.innerHTML = li;
+//   }
+  
+//   render_lists(users);
+  
+//   // lets filters it
+//   input = document.getElementById('filter_users');
+  
+//   let filterUsers = function(event){
+//     keyword = input.value.toLowerCase();
+//     filtered_users = users.filter(function(user){
+//           user = user.toLowerCase();
+//          return user.indexOf(keyword) > -1; 
+//     });
+    
+//     render_lists(filtered_users);
+//   }
+  
+//   input.addEventListener('keyup', filterUsers);
+  
+  
