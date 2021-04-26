8154bb0e37c7c25af7a32c4814195515486cb169
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-04-26 17:31:49 +0530

    handlebar for post

--- week23day2/server.js
+++ week23day2/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const path = require('path');
+const exphbs = require('express-handlebars');
 const PORT = 3000;
 
 app.use(express.urlencoded());
-app.use(express.json())
+app.use(express.json());
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', 'handlebars');
+
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Hello</h1>`)
+    res.render('home')
+});
+
+app.get('/post/:postId', async(req, res) => {
+    const postId = req.params.postId;
+    console.log("postId", postId);
+    const postResult = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .then((response) => response);
+    console.log("postResult", postResult);
+    res.render('post', { post : postResult });
 })
 
 app.listen(PORT, () => {
